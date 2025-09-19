29c35c2d3748b8dd5898ae79b1e1cab75a36a2e6
gokulanathan252 <gokulanathan252@gmail.com>
2025-09-19 14:28:02 +0530

    initial commit

--- hf.py
+++
@@ -1,9 +0,0 @@
-def add_two_numbers(a, b):
-    return a + b
-
-# Example usage
-if __name__ == "__main__":
-    num1 = 5
-    num2 = 7
-    result = add_two_numbers(num1, num2)
-    print(f"The sum of {num1} and {num2} is {result}")
